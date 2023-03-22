0ac861b3d4d82402264681a849f2d6a9affc137f
Lucas Mortelette <lucasmortelette62@gmail.com>
2023-03-22 15:45:38 +0100

    update et delete terminés

--- back/controllers/saucesController.js
+++ back/controllers/saucesController.js
@@ -1,4 +1,5 @@
 const Sauce = require("../models/sauces");
+const fs = require("fs");
 const multer = require("multer");
 const storage = multer.diskStorage({
   destination: "images/",
@@ -52,21 +53,59 @@ function getSauces_id(req, res) {
   }
 }
 
-function deleteSauce(req, res) {
+const deleteSauce = async (req, res) => {
   const id = req.params.id;
-  Sauce.remove({ _id: id }, (err, result) => {
-    if (err) return console.log(err);
-    res.redirect("/sauces");
+  await Sauce.findOne({ _id: id }).then((sauce) => {
+    const filename = sauce.imageUrl.split("3000/")[1];
+    fs.unlink(`../back/images/${filename}`, (err) => {
+      if (err) {
+        console.log(err);
+      }
+      Sauce.deleteOne({ _id: id })
+        .then(() => res.status(200).json({ message: "Sauce supprimée" }))
+        .catch((err) => res.status(400).json({ err }));
+    });
   });
-}
+};
 
 function updateSauce(req, res) {
   const hasNewImage = req.file != null;
-  if (hasNewImage) {
-    const body = req.body.sauce;
+  const id = req.params.id;
+  //on verifie si il y a une nouvelle image
+  if (hasNewImage === true) {
+    Sauce.findOne({ _id: id }).then((sauce) => {
+      const filename = sauce.imageUrl.split("3000/")[1];
+      //on supprime l'ancienne image à l'aide de son nom
+      fs.unlinkSync(`../back/images/${filename}`);
+      const body = JSON.parse(req.body.sauce);
+      const { userId, name, manufacturer, description, mainPepper, heat } =
+        body;
+      //on sauvegarde la nouvelle image et tous les autres changements
+      const imageUrl = "http://localhost:3000/" + req.file.filename;
+      const upsauce = Sauce.findByIdAndUpdate(
+        { _id: id },
+        {
+          $set: {
+            name: name,
+            manufacturer: manufacturer,
+            description: description,
+            mainPepper: mainPepper,
+            imageUrl: imageUrl,
+            heat: heat,
+          },
+        }
+      );
+      upsauce
+        .then((sauces) => res.send({ message: sauces }))
+        .catch((err) => console.log(err));
+    });
+  } else {
+    //si aucune nouvelle image, le contenu à recuperer est dans le req.body
+    const body = req.body;
     const { userId, name, manufacturer, description, mainPepper, heat } = body;
+    console.log(req.body);
     const id = req.params.id;
-    const imageUrl = "http://localhost:3000/" + req.file.filename;
+    //on met à jour toutes les infos sauf l'imageUrl
     const upsauce = Sauce.findByIdAndUpdate(
       { _id: id },
       {
@@ -75,36 +114,7 @@ function updateSauce(req, res) {
           manufacturer: manufacturer,
           description: description,
           mainPepper: mainPepper,
-          imageUrl: imageUrl,
           heat: heat,
-          likes: 0,
-          dislikes: 0,
-          usersLiked: [],
-          usersDisliked: [],
-        },
-      }
-    );
-    upsauce
-      .then((sauces) => res.send({ message: sauces }))
-      .catch((err) => console.log(err));
-  } else {
-    const body = req.body;
-    const { userId, name, manufacturer, description, mainPepper, heat } = body;
-    console.log(req.body);
-    const id = req.params.id;
-    const upsauce = Sauce.updateOne(
-      { _id: id },
-      {
-        $set: {
-          name: name,
-          manufacturer: manufacturer,
-          description: description,
-          mainPepper: mainPepper,
-          heat: heat,
-          likes: 0,
-          dislikes: 0,
-          usersLiked: [],
-          usersDisliked: [],
         },
       }
     );
--- back/routes/saucesRoutes.js
+++ back/routes/saucesRoutes.js
@@ -8,15 +8,7 @@ const router = Router();
 router.get("/api/sauces", token_verif, saucesController.getSauces);
 
 //Renvoie la sauce avec l'ID fourni
-router.get(
-  "/api/sauces/:id",
-  token_verif,
-  function (req, res, next) {
-    console.log("Request Id:", req.params.id);
-    next();
-  },
-  saucesController.getSauces_id
-);
+router.get("/api/sauces/:id", token_verif, saucesController.getSauces_id);
 
 //Capture en enregistre l'image, analyse la sauce et l'enregistre dans la DB, en definissant correctement son image URL. Remet les sauces aimées et celles détestées à 0, et les sauces userliked et celles userdilsiked aux tableaux vides
 router.post(
@@ -31,23 +23,11 @@ router.put(
   "/api/sauces/:id",
   token_verif,
   saucesController.upload.single("image"),
-  function (req, res, next) {
-    console.log("Request Id:", req.params.id);
-    next();
-  },
   saucesController.updateSauce
 );
 
 //Supprime la sauce avec l'ID fourni
-router.delete(
-  "/api/sauces/:id",
-  token_verif,
-  function (req, res, next) {
-    console.log("Request Id:", req.params.id);
-    next();
-  },
-  saucesController.deleteSauce
-);
+router.delete("/api/sauces/:id", token_verif, saucesController.deleteSauce);
 
 //Définit le statut "j'aime" pour UserID fourini.  Si j'aime = 0,l'utilisateur annule cequ'il aime ou ce qu'iln'aime pas. Si j'aime =-1, l'utilisateur n'aimepas la sauce.L'identifiant del'utilisateur doit êtreajouté ou supprimé dutableau approprié, en gardant une trace deses préférences et en l'empêchant d'aimer ou de ne pas aimer la même sauce plusieurs fois. Nombre total de "j'aime" et de "je n'aime pas" à mettre à jour avec chaque "j'aime"
 router.post("/api/sauces/:id/like", () => {});
